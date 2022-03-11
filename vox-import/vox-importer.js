75ac64fe99b7855cd60b312a96deeacc89fc2d0f
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-03-11 20:19:59 +0330

    Add grid descriptor to vox-importer

--- vox-import/vox-importer.js
+++ vox-import/vox-importer.js
@@ -50,7 +50,16 @@ function getDetails(voxels) {
 }
 
 async function main() {
-    const inputs = await rxjs.firstValueFrom(UtopiaApi.getInputsFromUser({inputs: baseParams}));
+    const inputs = await rxjs.firstValueFrom(UtopiaApi.getInputsFromUser({
+        inputs: baseParams,
+        gridDescriptor: {
+            rows: [
+              ["parserUrl"],
+              ["voxFile"],
+              ["startingPosition"]
+            ]
+        },
+    }));
     importScripts(inputs.parserUrl);
     const data = vox.parseMagicaVoxel(await inputs.voxFile._files[0].arrayBuffer());
 
