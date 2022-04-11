5abca31af4978d914f8285f198fda6beb381ebbb
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-04-11 18:39:28 +0430

    Add support for comma separated token ids in nft wall script

--- NftWall.js
+++ NftWall.js
@@ -82,15 +82,12 @@ const descriptor = {
                         name: "collection",
                         type: "text",
                         required: true,
-                        defaultValue:
-                            "0xdc0479cc5bba033b3e7de9f178607150b3abce1f",
                     },
                     {
                         label: "Token ID",
                         name: "tokenId",
-                        type: "number",
-                        required: true,
-                        defaultValue: "3380",
+                        type: "text",
+                        required: true
                     },
                 ],
                 gridDescriptor: {
@@ -212,6 +209,17 @@ async function main() {
 
     const nftWallData = [];
 
+    const items = [];
+    while(inputs.items.length > 0) {
+        const item = inputs.items.splice(0, 1)[0];
+        for (const tokenId of item.tokenId.replace(/\s*/gm, "").split(",")) {
+            items.push({
+                collection: item.collection,
+                tokenId: Number(tokenId)
+            })
+        }
+    }
+
     let usedRowsCount = 0;
     for (let y = 0; y < wallHeight; y++) {
         console.log("outer loop");
@@ -225,8 +233,8 @@ async function main() {
                 usedColumnsCount < inputs.columnsCount &&
                 usedRowsCount < inputs.rowsCount &&
                 isMetaCandidate(w, y) &&
-                inputs.items != null &&
-                inputs.items.length > 0
+                items != null &&
+                items.length > 0
             ) {
                 usedColumnsCount += 1;
                 metaBlock = {
@@ -234,7 +242,7 @@ async function main() {
                     properties: {},
                 };
 
-                const nft = inputs.items.splice(0, 1)[0];
+                const nft = items.splice(0, 1)[0];
                 metaBlock.properties[side] = {
                     collection: nft.collection,
                     tokenId: nft.tokenId,
@@ -265,9 +273,9 @@ async function main() {
     );
     console.log(JSON.stringify(result));
 
-    if (inputs.items.length > 0) {
+    if (items.length > 0) {
         console.warn(
-            `Could not place ${inputs.items.length} images. Choose wall corners carefully`
+            `Could not place ${items.length} images. Choose wall corners carefully`
         );
     }
 }
